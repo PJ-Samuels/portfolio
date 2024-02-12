040bc2e3ed03b6cc4530d121aa7ea0d990d5acf4
Oliver Samuels <osamuels@bu.edu>
2024-02-11 20:10:42 -0500

    new portfolio start

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import ViteSvgPlugin from 'vite-plugin-svg';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
