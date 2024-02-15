62c5077330088199c6f24cf51c049b0577b06700
Oliver Samuels <osamuels@bu.edu>
2024-02-15 11:13:20 -0500

    projects fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -58,7 +58,7 @@ function Home() {
 
   return (
   <main className="container mx-auto max-width section flex flex-col md:flex-row justify-center items-center">
-      <div className="text-center md:text-left md:w-1/2 pr-20">
+      <div className="text-center md:text-left md:w-1/2">
         <h1
           ref={h11}
           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
